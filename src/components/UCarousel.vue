1135eece612db13a5a696ab7b7ac1ee67f1b22ef
uedarap <uedarap@gmail.com>
2025-06-28 11:17:07 -0300

    UCarousel componente ✅

---
+++ src/components/UCarousel.vue
@@ -0,0 +1,92 @@
+<template>
+  <div style="width: 100%; height: 100%;">
+    <swiper
+      :slidesPerView="3"
+      :spaceBetween="30"
+      :freeMode="true"
+      :pagination="{
+        clickable: true,
+      }"
+      :modules="modules"
+      class="mySwiper"
+    >
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
+      <swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
+      <swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
+      <swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+    </swiper>
+  </div>
+</template>
+<script>
+  // Import Swiper Vue.js components
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+
+  // Import Swiper styles
+  import 'swiper/css';
+
+  import 'swiper/css/free-mode';
+  import 'swiper/css/pagination';
+
+  // import required modules
+  import { FreeMode, Pagination } from 'swiper/modules';
+
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      return {
+        modules: [FreeMode, Pagination],
+      };
+    },
+  };
+</script>
+
+<style scoped lang="scss">
+
+.swiper {
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #f5eaea;
+  height: 200px;
+
+  /* Center slide text vertically */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.mySwiper {
+  width: 100%;
+  /* deixe a altura automática para caber slides + paginação */
+  height: auto; 
+  padding-bottom: 40px; /* espaço para a paginação aparecer */
+}
+
+/* Força a paginação ficar no centro embaixo */
+.mySwiper .swiper-pagination {
+  bottom: 10px !important;
+  text-align: center;
+}
+
+// Deep selector para penetrar o scoped e aplicar o estilo nos elementos filhos que estão fora do escopo
+::v-deep(.swiper-pagination-bullet-active) {
+  background-color: #e73700 !important;
+  border-color: #fff !important;
+}
+
+</style>
--- src/views/index/Home.vue
+++ src/views/index/Home.vue
@@ -1,5 +1,7 @@
 <script setup>
 
+import U3Carousel from '../../components/UCarousel.vue';
+
 </script>
 
 <template>
@@ -715,14 +717,14 @@
 
   <!-- Modal -->
   <div class="modal fade" id="modalConsulta" tabindex="-1" aria-labelledby="modalConsultaLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="modalConsultaLabel">Consultas Médicas</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          Conteúdo do modal. Coloque aqui qualquer informação que deseja exibir.
+          <U3Carousel></U3Carousel>
         </div>
         <!-- <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
