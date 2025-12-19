de932bc9fca03c636ae5d390eaaca5703d64b0ce
uedarap <uedarap@gmail.com>
2025-12-19 09:12:00 -0300

    update

--- assets/index-BMY9dgNr.js
+++
@@ -1,1 +0,0 @@
-import{c as p,r}from"./router-C-B1nOZy.js";import o from"./Home-BIZT8N6o.js";p(o).use(r).mount("#app-home");
---
+++ assets/index-Nc1qJQWX.js
@@ -0,0 +1,1 @@
+import{c as p,r}from"./router-DkIKQuHC.js";import o from"./Home-D-Clmr_M.js";p(o).use(r).mount("#app-home");
