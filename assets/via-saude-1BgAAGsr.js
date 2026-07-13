90224f48c815c1d5a92cc39fee4027751fdae16a
Rapha Ueda <rueda@iportsolutions.com.br>
2026-07-13 15:24:54 -0300

    feat: add new Via Saúde module and update related assets
    - Introduced a new JavaScript file for the Via Saúde feature (via-saude-1BgAAGsr.js) and removed the old version (via-saude-CsyCJU7W.js).
    - Updated index.html to reference the new JavaScript and CSS files for the Via Saúde feature.
    - Created a new index.html file for the Via Saúde module with necessary links and structure.
    - Adjusted the main index.html to point to the correct main.js file for the application.

---
+++ assets/via-saude-1BgAAGsr.js
@@ -0,0 +1,1 @@
+import{c as r,r as a}from"./router-CpWEnTr8.js";import m from"./ViaSaude-WUqPwgo9.js";r(m).use(a).mount("#via-saude");
--- assets/via-saude-CsyCJU7W.js
+++
@@ -1,1 +0,0 @@
-import{a as r,r as a}from"./router-DzWKCj3p.js";import m from"./ViaSaude-BaYBaS3f.js";r(m).use(a).mount("#via-saude");
