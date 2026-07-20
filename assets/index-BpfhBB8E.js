aea6b54816701cd10064eebf4e9a98789f6ffb0a
uedarap <uedarap@gmail.com>
2026-07-20 18:48:54 -0300

    feat: replace via-saude-DwH-KMUg.js with via-saude-B0YE_muX.js and update index.html references
    - Added new JavaScript file via-saude-B0YE_muX.js for Vue app initialization.
    - Removed deprecated JavaScript file via-saude-DwH-KMUg.js.
    - Updated index.html to reference new assets and ensure proper loading of styles and scripts.
    - Modified via-saude/index.html to reflect changes in script and stylesheet links.
    - Enhanced styling in ViaSaude.vue by adjusting overflow and image positioning for better layout.

--- assets/index-BpfhBB8E.js
+++
@@ -1,1 +0,0 @@
-import{c as p,r}from"./router-tJ-s-sPG.js";import o from"./Home-C1jwuCz2.js";p(o).use(r).mount("#app-home");
---
+++ assets/via-saude-B0YE_muX.js
@@ -0,0 +1,1 @@
+import{c as r,r as a}from"./router-OlvBUNfb.js";import m from"./ViaSaude-DBKiSUpE.js";r(m).use(a).mount("#via-saude");
