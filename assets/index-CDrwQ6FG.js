769a0c165a73b97803eff4b9229f45e7d5b2ab37
Rapha Ueda <rueda@iportsolutions.com.br>
2026-07-20 12:18:12 -0300

    Add Via Saúde module and HTML entry point
    - Created a new JavaScript file for the Via Saúde module, importing necessary components and mounting the application.
    - Added an HTML file for the Via Saúde page, including meta tags, styles, and script references for proper functionality.

---
+++ assets/index-CDrwQ6FG.js
@@ -0,0 +1,1 @@
+import{c as p,r}from"./router-CNlIC98G.js";import o from"./Home-C-rX5YAp.js";p(o).use(r).mount("#app-home");
---
+++ assets/via-saude-B5Af69wZ.js
@@ -0,0 +1,1 @@
+import{c as r,r as a}from"./router-CNlIC98G.js";import m from"./ViaSaude-zCxnv8Pm.js";r(m).use(a).mount("#via-saude");
