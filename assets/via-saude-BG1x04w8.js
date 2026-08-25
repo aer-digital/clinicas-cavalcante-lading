24ca2046975f183e634483d0fa0529773790223a
uedarap <uedarap@gmail.com>
2026-08-25 18:41:18 -0300

    feat: update via-saude assets and integrate Google Tag Manager
    - Removed obsolete via-saude-BG1x04w8.js file.
    - Added new via-saude-BavpJCp1.js file for improved functionality.
    - Updated index.html to include Google Tag Manager for enhanced tracking.
    - Created a new via-saude view with its own HTML structure.
    - Updated main index.html to reflect Google Tag Manager integration.
    - Bumped version to 2.0.5 in package.json.
    - Minor adjustments to coming-soon view for consistency.

--- assets/via-saude-BG1x04w8.js
+++
@@ -1,1 +0,0 @@
-import{c as r,r as a}from"./router-CjAfVZwE.js";import m from"./ViaSaude-CnzOJ03F.js";r(m).use(a).mount("#via-saude");
---
+++ assets/via-saude-BavpJCp1.js
@@ -0,0 +1,1 @@
+import{c as r,r as a}from"./router-Cf9hKxpN.js";import m from"./ViaSaude-fqm8rxqA.js";r(m).use(a).mount("#via-saude");
